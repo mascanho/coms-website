56e235224e334775af13549cdd92d63813137e2d
mascanho <mascanho@sapo.pt>
2024-05-02 18:16:02 +0100

    Responsiveness fixes

--- components/About.tsx
+++ components/About.tsx
@@ -2,7 +2,7 @@ import TimelineIMS from "./Timeline";
 
 function About() {
   return (
-    <section className="max-w-5xl mx-auto  m:mt-0 mt-16 ">
+    <section className="max-w-5xl w-11/12 mx-auto  m:mt-0 mt-16 ">
       <div className="mx-auto px-4 sm:px-0 py-2 my-20 sm:py-1 ">
         <div className="grid grid-cols-1 gap-8 lg:grid-cols-2 lg:gap-16">
           <div className="relative mt-5 sm:mt-0  h-full overflow-hidden order-last rounded-lg sm:h-80 lg:order-last lg:h-full flex items-center justify-center">
